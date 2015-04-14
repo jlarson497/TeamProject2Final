b8f6ba3031365e46e9f63411ac2f734acd15f79c
jlarson497 <kf2617gi@go.minneapolis.edu>
2015-04-14 15:56:46 -0500

    Moved weather to its own class -- handled getWeather button using new methods

--- Team 2 Final/Form1.vb
+++ Team 2 Final/Form1.vb
@@ -7,6 +7,9 @@ Imports System.Xml
 Public Class Form1
 
     Dim formMap As New oMap
+    Dim formWeather As New oWeather
+
+
 
     Private Sub btnGetMap_Click(sender As Object, e As EventArgs) Handles btnGetMap.Click
         Dim city As String = txtBoxCityName.Text
@@ -15,112 +18,41 @@ Public Class Form1
 
     End Sub
 
-    'Weather
 
-    ' build a web request for 3 day forecast off of this.
-    ' http://api.wunderground.com/api/ede6553743c4d570/forecast/q/MO/St_Louis.xml
-    ' a function that returns the vlaue of the txtbox input
-    Public Function retrieveTxtboxCity() As String
-        Dim cityFromtxtBox As String = txtBoxCityName.Text
-        If txtBoxCityName.Text = "" Or txtBoxCityName.Text = Nothing Then
-            MessageBox.Show("Please enter a city name ", "Missing Info", MessageBoxButtons.OK, MessageBoxIcon.Hand)
-            Stop
-        Else
-            Return cityFromtxtBox
-        End If
-        '' both city and state could return a null reference exception on run time. How can i fix this ?
-    End Function
 
-    'retrieves the state from the comob box and handles not selecting errors
-    Public Function retrieveState() As String
-        Dim state As String = stateComboBox.Text.ToString
-        If stateComboBox.Text = Nothing Then
-            MessageBox.Show("Select a state from the combo dropdown box", "Missing Info", MessageBoxButtons.OK, MessageBoxIcon.Hand)
-        Else
-            Return state
-        End If
-    End Function
 
-    '' the function that builds the url request from user input
-    Public Function url_request_built() As String
-        'Dim test_url As String = "http://api.wunderground.com/api/ede6553743c4d570/forecast/q/MO/St_Louis.xml"
-        Try
-            Dim ThreeDayForecastRequest As String = "http://api.wunderground.com/api/ede6553743c4d570/forecast/q" + "/" + retrieveState() + "/" + retrieveTxtboxCity() + ".xml"
-            Return ThreeDayForecastRequest
-        Catch problemBuildintUrl As Exception
-            MessageBox.Show("Problem generating webrequest. Check that all data has been entered correctly", "Error processing request", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        End Try
-    End Function
+
+
 
 
     '' as a background worker i should be able to call diffferent web request. So they should be queued up here.
     Private Sub apiBackgroundWorker_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles apiBackgroundWorker.DoWork
 
     End Sub
+
     Private Sub txtBoxCityName_Click(sender As Object, e As EventArgs) Handles txtBoxCityName.Click
         txtBoxCityName.Clear()
     End Sub
 
 
     Private Sub btnGetWeather_Click(sender As Object, e As EventArgs) Handles btnGetWeather.Click
-        ' the test url
-        ' Try
-        'the web request
-        Dim webRequestForecast As WebRequest = WebRequest.Create(url_request_built())
-        '' comes back as a stream, prepare a variable to handle/process stream
-        Dim forecastResponseStream As Stream = webRequestForecast.GetResponse.GetResponseStream()
-        Dim xmlWeatherReader As New XmlDocument
-        xmlWeatherReader.Load(forecastResponseStream)
-        '' from the response stream i need to be able to read.
-        Dim xmlDay As XmlNode = xmlWeatherReader.SelectSingleNode("//title")
-        '' //title as day of the week
-        'trying to access a specific day in the returned list of title elements. The 3rd day is what i want.
-        Dim xmlday2 As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[3]/title")
-        Dim xmlNodeDay2url_icon As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[3]/icon_url")
-        Dim xmlday2Description As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[3]/icon")
-        lblday2.Text = xmlday2.InnerText
-        lblDescription2.Text = xmlday2Description.InnerText
-        Dim xmlday3 As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[5]/title")
-        Dim xmlNodeDay3url_icon As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[5]/icon_url")
-        Dim xmlday3Description As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[5]/icon")
-        lblday3.Text = xmlday3.InnerText
-        lblDescription3.Text = xmlday3Description.InnerText
-        '' //icon for a brief description of the weather
-        Dim xmlIconDescription As XmlNode = xmlWeatherReader.SelectSingleNode("//icon")
-        '' //icon_url as the icon image to read from the stream
-        'MessageBox.Show("day 2 " + xmlday2.InnerXml)
-        Dim xmlIconRequestURL As XmlNode = xmlWeatherReader.SelectSingleNode("//icon_url")
-        lblday.Text = xmlDay.InnerText
-        ''// fctext equals forecast text description
-        lblDescription1.Text = xmlIconDescription.InnerText
-        ' Catch any exceptions.
-        ' Catch problemReadingXML As Exception
-        ' MessageBox.Show("There was a problem Reading the xml file. " + vbCrLf + "Check to make sure you are connectedt to the internet ", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        ' End Try
-        'testLabel.Text = xmlIconRequestURL.InnerText
-        '' now get the image for the forecast and draw it to the picture box
-        '' this chunk of code becomes inacessable from previous try catch statements. removed for testing
-        Try
-            Dim icon_url As String = xmlIconRequestURL.InnerText
-            Dim icon2 As String = xmlNodeDay2url_icon.InnerText
-            Dim icon3 As String = xmlNodeDay3url_icon.InnerText
-            Dim iconWebRequest As WebRequest = WebRequest.Create(icon_url)
-            Dim webimageResonse As Stream = iconWebRequest.GetResponse.GetResponseStream()
-            Dim weatherDay1Icon As Image = Image.FromStream(webimageResonse)
-            forecastImageBox1.Image = weatherDay1Icon
-            Dim iconWebRequest2 As WebRequest = WebRequest.Create(icon2)
-            Dim webimageResonse2 As Stream = iconWebRequest2.GetResponse.GetResponseStream()
-            Dim weatherDayIcon2 As Image = Image.FromStream(webimageResonse2)
-            forecastImageBox2.Image = weatherDayIcon2
-            Dim iconWebRequest3 As WebRequest = WebRequest.Create(icon3)
-            Dim webimageResonse3 As Stream = iconWebRequest3.GetResponse.GetResponseStream()
-            Dim weatherDayIcon3 As Image = Image.FromStream(webimageResonse3)
-            forecastImageBox3.Image = weatherDayIcon3
-            'Dim g As Graphics
-            'forcastImageBox = g.DrawImage(Image.FromStream(weatherDay1Icon), New Rectangle(0, 0, forcastImageBox.Width, forcastImageBox.Height))
-        Catch ex As WebException
-            MessageBox.Show(ex.Message)
-        End Try
+        Dim formWeather As New oWeather
+        Dim city As String = txtBoxCityName.Text
+        Dim state As String = stateComboBox.Text
+        formWeather.getWeather(city, state)
+        lblday.Text = formWeather.getForecastday(city, state, 1)
+        lblday2.Text = formWeather.getForecastday(city, state, 3)
+        lblday3.Text = formWeather.getForecastday(city, state, 5)
+
+        forecastImageBox1.Image = formWeather.getForcastIcon(city, state, 1)
+        forecastImageBox2.Image = formWeather.getForcastIcon(city, state, 3)
+        forecastImageBox3.Image = formWeather.getForcastIcon(city, state, 5)
+
+        lblDescription1.Text = formWeather.getForcastDescription(city, state, 1)
+        lblDescription2.Text = formWeather.getForcastDescription(city, state, 3)
+        lblDescription3.Text = formWeather.getForcastDescription(city, state, 5)
+
+
     End Sub
 
     'Flickr
---
+++ Team 2 Final/oWeather.vb
@@ -0,0 +1,86 @@
+﻿Imports System.Net
+Imports System.Xml
+Imports System.IO
+
+Public Class oWeather
+    'weather class
+
+    ' build a web request for 3 day forecast off of this.
+    ' http://api.wunderground.com/api/ede6553743c4d570/forecast/q/MO/St_Louis.xml
+    ' a function that returns the vlaue of the txtbox input
+
+
+    '' the function that builds the url request from user input
+    Public Function url_request_built(city As String, state As String) As String
+        'Dim test_url As String = "http://api.wunderground.com/api/ede6553743c4d570/forecast/q/MO/St_Louis.xml"
+        Try
+            Dim ThreeDayForecastRequest As String = "http://api.wunderground.com/api/ede6553743c4d570/forecast/q" & "/" & state + "/" + city + ".xml"
+            Return ThreeDayForecastRequest
+        Catch problemBuildintUrl As Exception
+            MessageBox.Show("Problem generating webrequest. Check that all data has been entered correctly", "Error processing request", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return 0
+        End Try
+    End Function
+
+
+    '' as a background worker i should be able to call diffferent web request. So they should be queued up here.
+
+    Public Sub getWeather(city As String, state As String)
+        ' the test url
+        ' Try
+        'the web request
+        Dim webRequestForecast As WebRequest = WebRequest.Create(url_request_built(city, state))
+        '' comes back as a stream, prepare a variable to handle/process stream
+        Dim forecastResponseStream As Stream = webRequestForecast.GetResponse.GetResponseStream()
+        Dim xmlWeatherReader As New XmlDocument
+        xmlWeatherReader.Load(forecastResponseStream)
+        '' from the response stream i need to be able to read.
+
+    End Sub
+
+    Public Function getForecastday(city As String, state As String, day As Integer)
+        Dim webRequestForecast As WebRequest = WebRequest.Create(url_request_built(city, state))
+        '' comes back as a stream, prepare a variable to handle/process stream
+        Dim forecastResponseStream As Stream = webRequestForecast.GetResponse.GetResponseStream()
+        Dim xmlWeatherReader As New XmlDocument
+        xmlWeatherReader.Load(forecastResponseStream)
+        '' from the response stream i need to be able to read.
+        Dim xmlday As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[" & day & "]/title")
+        Dim daytoreturn As String = xmlday.InnerText
+
+        Return daytoreturn
+
+
+    End Function
+
+    Public Function getForcastIcon(city As String, state As String, day As Integer)
+        Dim webRequestForecast As WebRequest = WebRequest.Create(url_request_built(city, state))
+        '' comes back as a stream, prepare a variable to handle/process stream
+        Dim forecastResponseStream As Stream = webRequestForecast.GetResponse.GetResponseStream()
+        Dim xmlWeatherReader As New XmlDocument
+        xmlWeatherReader.Load(forecastResponseStream)
+        '' from the response stream i need to be able to read.
+        Dim xmlIcon As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[" & day & "]/icon_url")
+        Dim icon_url As String = xmlIcon.InnerText
+        Dim iconWebRequest As WebRequest = WebRequest.Create(icon_url)
+        Dim webimageResonse As Stream = iconWebRequest.GetResponse.GetResponseStream()
+        Dim icontoreturn As Image = Image.FromStream(webimageResonse)
+        Return icontoreturn
+
+    End Function
+
+    Public Function getForcastDescription(city As String, state As String, day As Integer)
+        Dim webRequestForecast As WebRequest = WebRequest.Create(url_request_built(city, state))
+        '' comes back as a stream, prepare a variable to handle/process stream
+        Dim forecastResponseStream As Stream = webRequestForecast.GetResponse.GetResponseStream()
+        Dim xmlWeatherReader As New XmlDocument
+        xmlWeatherReader.Load(forecastResponseStream)
+        '' from the response stream i need to be able to read.
+        Dim xmlDescription As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[" & day & "]/icon")
+        Dim descriptiontoreturn As String = xmlDescription.InnerText
+
+        Return descriptiontoreturn
+
+    End Function
+
+End Class
